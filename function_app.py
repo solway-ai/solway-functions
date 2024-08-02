31bd128eeff6896b29384fbf82ce70c16df815a7
Kristian Berg <kris.as.berg@gmail.com>
2024-08-01 21:59:10 -0600

    creds

--- function_app.py
+++ function_app.py
@@ -10,30 +10,3 @@ app = func.AsgiFunctionApp(
     app=api, 
     http_auth_level=func.AuthLevel.ANONYMOUS
 )
-
-#app = func.FunctionApp()
-# @app.route(route="HttpExample", auth_level=func.AuthLevel.ANONYMOUS)
-# def HttpExample(req: func.HttpRequest) -> func.HttpResponse:
-#     logging.info('Python HTTP trigger function processed a request.')
-#     return func.HttpResponse("Hello, world!", status_code=200)
-
-# @app.route(route="HttpExample")
-# def HttpExample(req: func.HttpRequest) -> func.HttpResponse:
-#     logging.info('Python HTTP trigger function processed a request.')
-
-#     name = req.params.get('name')
-#     if not name:
-#         try:
-#             req_body = req.get_json()
-#         except ValueError:
-#             pass
-#         else:
-#             name = req_body.get('name')
-
-#     if name:
-#         return func.HttpResponse(f"Hello, {name}. This HTTP triggered function executed successfully.")
-#     else:
-#         return func.HttpResponse(
-#              "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
-#              status_code=200
-#         )
