6d47c0bba833f9da14da131c9dd95dd66f430f44
Kristian Berg <kris.as.berg@gmail.com>
2024-08-02 17:01:52 -0600

    requirements.txt

--- function_app.py
+++ function_app.py
@@ -1,3 +1,5 @@
+import sys
+import os
 import logging
 import azure.functions as func
 
@@ -12,12 +14,19 @@ app = func.FunctionApp(http_auth_level=func.AuthLevel.ANONYMOUS)
 
 @app.route(route="HttpTrigger", auth_level=func.AuthLevel.ANONYMOUS)
 def HttpTrigger(req: func.HttpRequest) -> func.HttpResponse:
+
+
+    logging.info(f'Sys path: {sys.path}')
+    logging.info(f'Python version: {sys.version}')
+    logging.info(f'Installed packages: {os.system("pip freeze")}')
+
     try:
         from SolwayAPI.api.v1.core.chain_prompts import research_questions
-        logging.info('Imported successfully.')
-        return {
-            "prompt": research_questions
-        }
+
+        logging.info('Imported research_questions successfully.')
+        logging.info(str(research_questions))
+        return func.HttpResponse(f"Succes: {research_questions}", status_code=200)
+    
     except ImportError as e:
         logging.error(f'Failed to import: {e}')
         return func.HttpResponse(f"Import error: {str(e)}", status_code=500)
