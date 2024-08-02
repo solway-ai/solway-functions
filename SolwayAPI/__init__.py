cc09cd149a8fbe06c8e63007e486ba71292413d3
Kristian Berg <kris.as.berg@gmail.com>
2024-08-02 00:02:52 -0600

    init

---
+++ SolwayAPI/__init__.py
@@ -0,0 +1,18 @@
+from fastapi import (
+    FastAPI, 
+)
+
+from SolwayAPI.api.v1.api import api_router
+from SolwayAPI.api.v1.core.config import settings
+
+app = FastAPI(
+    title=settings.PROJECT_NAME,
+    version=settings.PROJECT_VERSION,
+    tags=['main']
+)
+
+app.include_router(api_router)
+
+@app.get("/") 
+async def main():     
+    return {"Solway AI Engineering and Consulting API"}
