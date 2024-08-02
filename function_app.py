40d8b7dac3adb6c0db2cbc27f973cb4ee132ccd3
Kristian Berg <kris.as.berg@gmail.com>
2024-08-01 23:11:53 -0600

    rebuild

--- function_app.py
+++ function_app.py
@@ -1,3 +1,4 @@
+import logging
 import azure.functions as func
 
 from SolwayAPI.main import app as api
@@ -6,3 +7,13 @@ app = func.AsgiFunctionApp(
     app=api, 
     http_auth_level=func.AuthLevel.ANONYMOUS
 )
+
+
+# app = func.FunctionApp(http_auth_level=func.AuthLevel.ANONYMOUS)    
+
+# @app.function_name("personas")
+
+#     @app.route(route="character-managment/personas")
+#     def personas(req: func.HttpRequest) -> func.HttpResponse:
+#         logging.info('Python HTTP trigger function processed a request.')
+#         return func.HttpResponse("ok", status_code=200)
