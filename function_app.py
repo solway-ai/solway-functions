9e6a7818378663fb697491b16f3acf7108332812
Kristian Berg <kris.as.berg@gmail.com>
2024-08-02 15:40:39 -0600

    app

--- function_app.py
+++ function_app.py
@@ -3,10 +3,10 @@ import azure.functions as func
 
 from SolwayAPI import app as api
 
-app = func.AsgiFunctionApp(
-    app=api, 
-    http_auth_level=func.AuthLevel.ANONYMOUS
-)
+# app = func.AsgiFunctionApp(
+#     app=api, 
+#     http_auth_level=func.AuthLevel.ANONYMOUS
+# )
 
 app = func.FunctionApp(http_auth_level=func.AuthLevel.ANONYMOUS)
 
@@ -24,7 +24,7 @@ def HttpTrigger(req: func.HttpRequest) -> func.HttpResponse:
             name = req_body.get('name')
 
     if name:
-        return func.HttpResponse(f"Hello, {name}. This HTTP triggered function executed successfully with {arr} and {df}.")
+        return func.HttpResponse(f"Hello, {name}. This HTTP triggered function executed successfully")
     else:
         return func.HttpResponse(
              "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
