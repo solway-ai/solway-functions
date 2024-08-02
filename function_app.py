46caca2871239f15e35175424b1a1d3940a10581
Kristian Berg <kris.as.berg@gmail.com>
2024-08-02 15:34:08 -0600

    app

--- function_app.py
+++ function_app.py
@@ -1,32 +1,32 @@
 import logging
 import azure.functions as func
 
-#from SolwayAPI import app as api
+from SolwayAPI import app as api
 
-# app = func.AsgiFunctionApp(
-#     app=api, 
-#     http_auth_level=func.AuthLevel.ANONYMOUS
-# )
+app = func.AsgiFunctionApp(
+    app=api, 
+    http_auth_level=func.AuthLevel.ANONYMOUS
+)
 
-app = func.FunctionApp(http_auth_level=func.AuthLevel.ANONYMOUS)
+# app = func.FunctionApp(http_auth_level=func.AuthLevel.ANONYMOUS)
 
-@app.route(route="HttpTrigger", auth_level=func.AuthLevel.ANONYMOUS)
-def HttpTrigger(req: func.HttpRequest) -> func.HttpResponse:
-    logging.info('Python HTTP trigger function processed a request.')
+# @app.route(route="HttpTrigger", auth_level=func.AuthLevel.ANONYMOUS)
+# def HttpTrigger(req: func.HttpRequest) -> func.HttpResponse:
+#     logging.info('Python HTTP trigger function processed a request.')
 
-    name = req.params.get('name')
-    if not name:
-        try:
-            req_body = req.get_json()
-        except ValueError:
-            pass
-        else:
-            name = req_body.get('name')
+#     name = req.params.get('name')
+#     if not name:
+#         try:
+#             req_body = req.get_json()
+#         except ValueError:
+#             pass
+#         else:
+#             name = req_body.get('name')
 
-    if name:
-        return func.HttpResponse(f"Hello, {name}. This HTTP triggered function executed successfully with {arr} and {df}.")
-    else:
-        return func.HttpResponse(
-             "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
-             status_code=200
-        )
+#     if name:
+#         return func.HttpResponse(f"Hello, {name}. This HTTP triggered function executed successfully with {arr} and {df}.")
+#     else:
+#         return func.HttpResponse(
+#              "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
+#              status_code=200
+#         )
