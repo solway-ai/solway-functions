26816073b067a6220f2d06967f30a8f4d32a4a1c
Kristian Berg <kris.as.berg@gmail.com>
2024-08-12 14:57:09 -0600

    imports

--- function_app.py
+++ function_app.py
@@ -3,32 +3,33 @@ import os
 import logging
 import azure.functions as func
 
-from SolwayAPI import app as api
+#from SolwayAPI import app as api
 
-app = func.AsgiFunctionApp(
-    app=api, 
-    http_auth_level=func.AuthLevel.ANONYMOUS
-)
+# app = func.AsgiFunctionApp(
+#     app=api, 
+#     http_auth_level=func.AuthLevel.ANONYMOUS
+# )
 
-# app = func.FunctionApp(http_auth_level=func.AuthLevel.ANONYMOUS)
+app = func.FunctionApp(http_auth_level=func.AuthLevel.ANONYMOUS)
 
-# logging.basicConfig(level=logging.INFO)
+logging.basicConfig(level=logging.INFO)
 
-# @app.route(route="HttpTrigger", auth_level=func.AuthLevel.ANONYMOUS)
-# def HttpTrigger(req: func.HttpRequest) -> func.HttpResponse:
+@app.route(route="HttpTrigger", auth_level=func.AuthLevel.ANONYMOUS)
+def HttpTrigger(req: func.HttpRequest) -> func.HttpResponse:
 
 
-#     logging.info(f'Sys path: {sys.path}')
-#     logging.info(f'Python version: {sys.version}')
-#     logging.info(f'Installed packages: {os.system("pip freeze")}')
+    logging.info(f'Sys path: {sys.path}')
+    logging.info(f'Python version: {sys.version}')
+    logging.info(f'Installed packages: {os.system("pip freeze")}')
 
-#     try:
-#         from SolwayAPI.api.v1.core.chain_prompts import research_questions
+    try:
+        from SolwayAPI.api.v1.core.chain_prompts import research_questions
+        from SolwayAPI.api.v1.resources.notion import create_child_notion_page
 
-#         logging.info('Imported research_questions successfully.')
-#         logging.info(str(research_questions))
-#         return func.HttpResponse(f"Succes: {research_questions}", status_code=200)
+        logging.info('Imported research_questions successfully.')
+        logging.info(str(research_questions))
+        return func.HttpResponse(f"Succes: {research_questions}", status_code=200)
     
-#     except ImportError as e:
-#         logging.error(f'Failed to import: {e}')
-#         return func.HttpResponse(f"Import error: {str(e)}", status_code=500)
+    except ImportError as e:
+        logging.error(f'Failed to import: {e}')
+        return func.HttpResponse(f"Import error: {str(e)}", status_code=500)
